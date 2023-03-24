50402fc61675bb14251a82f7f242e0145fb68d5a
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-24 01:51:30 +0100

    asdas

--- modules.py
+++ modules.py
@@ -1,27 +1,29 @@
 import pymem
 import pymem.process
-import time
 import sys
 from requests import get
 
 
+
+def getoffsets():
+    try:
+        haze = get(
+            "https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.json"
+        ).json()
+
+        [setattr(Offsets, k, v) for k, v in haze["signatures"].items()]
+        [setattr(Offsets, k, v) for k, v in haze["netvars"].items()]
+        print("offsets fetched succesfuly")
+    except:
+        sys.exit("Unable to fetch Hazedumper's Offsets")
+
+
 class Offsets:
+   
     pass
+getoffsets()
 
-def offsets():
-        try:
-            haze = get(
-                "https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.json"
-            ).json()
-
-            [setattr(Offsets, k, v) for k, v in haze["signatures"].items()]
-            [setattr(Offsets, k, v) for k, v in haze["netvars"].items()]
-        except:
-            sys.exit("Unable to fetch Hazedumper's Offsets")
-
-
-
-def base():
+class base():
     pm = pymem.Pymem("csgo.exe")
     client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
     pass
--- noflash.py
+++ noflash.py
@@ -1,12 +1,12 @@
-from modules import Offsets, base
+from modules import offsets, base
 import time
 
 
 def noflash():
     while True:
-            player = base.pm.read_int(base.client + Offsets.dwLocalPlayer)
+            player = base.pm.read_int(base.client + offsets.dwLocalPlayer)
             if player:
-                flash_value = base.player + Offsets.m_flFlashMaxAlpha
+                flash_value = base.player + offsets.m_flFlashMaxAlpha
                 if flash_value:
                     base.pm.write_float(flash_value, float(0))
             time.sleep(1)
