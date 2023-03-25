efd6121814af9b157db59ad0616ff070570bb97d
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-25 16:21:02 +0100

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -5,10 +5,11 @@ from modules import *
 
 
 def trigger():
-    while(base.pm.r_int(base.proc, base.localplayer + Offsets.m_fFlags) == 257) and base.pm.key_pressed(86):
-                base.pm.w_int(base.pm , base.client + Offsets.dwForceAttack, 1)
+    while True:
+        if keyboard.is_pressed("v"):
+                base.pm.write_int(base.client + Offsets.dwForceAttack, 1)
                 time.sleep(0.1)
-                base.pm.w_int(base.pm , base.client + Offsets.dwForceAttack, 0)
+                base.pm.write_int(base.client + Offsets.dwForceAttack, 0)
 
 
 
