1c49da8b3227d3563fad49746d81251dfd80a2ab
ax11 <125132362+ax11s@users.noreply.github.com>
2023-03-26 14:29:03 +0200

    Update trigger.py

--- trigger.py
+++ trigger.py
@@ -7,7 +7,7 @@ crosshairid = base.client + Offsets.m_iCrosshairId
 
 def trigger():
     while True:
-        if keyboard.is_pressed("v") and 0 < base.pm.read_int(base.localplayer + Offsets.m_iCrosshairId):
+        if keyboard.is_pressed("c") and 0 < base.pm.read_int(base.localplayer + Offsets.m_iCrosshairId):
                     base.pm.write_int(base.client + Offsets.dwForceAttack, 1)
                     time.sleep(0.5)
                     base.pm.write_int(base.client + Offsets.dwForceAttack, 0)
