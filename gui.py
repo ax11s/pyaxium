ccb689a15dd578d19996a038524f02bfab03186f
ax11 <125132362+ax11s@users.noreply.github.com>
2023-04-01 19:01:04 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -25,16 +25,23 @@ class App(customtkinter.CTk):
 
 
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=5, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(5, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Pyaxium", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(30, 20))
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame,text="Stats", command=self.stats)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
-        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
-        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+
+
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame,text="Aiming", command=self.aiming)
+        self.sidebar_button_1.grid(row=2, column=0, padx=20, pady=10)
+
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame,text="Visuals", command=self.visuals)
+        self.sidebar_button_2.grid(row=3, column=0, padx=20, pady=10)
+        
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame,text="Utilities", command=self.Utilities)
+        self.sidebar_button_3.grid(row=4, column=0, padx=20, pady=10)
 
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"], command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 20))
@@ -48,10 +55,25 @@ class App(customtkinter.CTk):
                     self.label2.place(relx=0.5, rely=0.26, anchor=tkinter.CENTER)
                     self.label3 = customtkinter.CTkLabel(self.frame, text="Keep in mind that its a 'legit' client, made to be a help for you to win and not be used as a hvh cheat.", font=('', 14, 'normal'))
                     self.label3.place(relx=0.5, rely=0.35, anchor=tkinter.CENTER)
-                    
 
-    def sidebar_button_event(self):
-        print("sidebar_button click")
+
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+
+    def stats(self):
+        print("stats")
+
+    def aiming(self):
+        print("aiming")
+
+    def visuals(self):
+        print("visuals")
+
+
+    def Utilities(self):
+        print("Utilities")
 
 
 if __name__ == "__main__":
