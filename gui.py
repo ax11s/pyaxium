db0c62466b995c8e27cc4b8438c1d41e4a1b4127
ax11 <125132362+ax11s@users.noreply.github.com>
2023-04-05 22:31:31 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -58,7 +58,7 @@ class App(customtkinter.CTk):
                                 self.frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
                                 self.label = customtkinter.CTkLabel(self.frame, text='Welcome to pyaxium!', font=('', 18, 'bold'))
                                 self.label.place(relx=0.5, rely=0.12, anchor=tkinter.CENTER)
-                                self.label2 = customtkinter.wCTkLabel(self.frame, text="Welcome to the cheat suite for cs:go made by ax11! I hope you will have fun using this client.", font=('', 14, 'normal'))
+                                self.label2 = customtkinter.CTkLabel(self.frame, text="Welcome to the cheat suite for cs:go made by ax11! I hope you will have fun using this client.", font=('', 14, 'normal'))
                                 self.label2.place(relx=0.5, rely=0.26, anchor=tkinter.CENTER)
                                 self.label3 = customtkinter.CTkLabel(self.frame, text="Keep in mind that its a 'legit' client, made to be a help for you to win and not be used as a hvh cheat.", font=('', 14, 'normal'))
                                 self.label3.place(relx=0.5, rely=0.35, anchor=tkinter.CENTER)
